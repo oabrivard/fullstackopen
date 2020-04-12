ff73270f3d51aef11826a7fa5a1adb1107098b67
oabrivard <olivier@abrivard.fr>
2020-04-12 19:06:09 -0400

    updated 1.4: course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -21,7 +21,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  return (<p>Number of exercises {props.total}</p>)
+  return (<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>)
 }
 
 const App = () => {
@@ -45,7 +45,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Total parts={parts} />
     </div>
   )
 }
