10d1aa52a772ed675c6fc0c9a0beab6e43576037
oabrivard <olivier@abrivard.fr>
2020-04-18 01:08:32 -0400

    finished 1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom'
 const Header = ({name}) => <h1>{name}</h1>
 const Button = ({clickHandler,text}) => <button onClick={clickHandler}>{text}</button>
 const Counter = ({text,count}) => <div>{text} {count}</div>
+const Average = ({value}) => <div>average {value}</div>
+const Positive = ({value}) => <div>positive {value * 100}%</div>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -20,6 +23,9 @@ const App = () => {
       <Counter text="good" count={good} />
       <Counter text="neutral" count={neutral} />
       <Counter text="bad" count={bad} />
+      <Counter text="all" count={good + neutral + bad} />
+      <Average value={(good - bad) / (good + neutral + bad)} />
+      <Positive value={good / (good + neutral + bad)} />
     </div>
   )
 }
