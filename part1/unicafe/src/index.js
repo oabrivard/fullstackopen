56b7b86b2fb66c296f03480067b19951e49d5eeb
oabrivard <olivier@abrivard.fr>
2020-04-18 01:15:29 -0400

    finished 1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,15 @@ const Counter = ({text,count}) => <div>{text} {count}</div>
 const Average = ({value}) => <div>average {value}</div>
 const Positive = ({value}) => <div>positive {value * 100}%</div>
 
+const Statistics = ({good,neutral,bad}) => <div>
+      <Counter text="good" count={good} />
+      <Counter text="neutral" count={neutral} />
+      <Counter text="bad" count={bad} />
+      <Counter text="all" count={good + neutral + bad} />
+      <Average value={(good - bad) / (good + neutral + bad)} />
+      <Positive value={good / (good + neutral + bad)} />
+</div>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -20,12 +29,7 @@ const App = () => {
       <Button clickHandler={() => setNeutral(neutral + 1)} text="neutral" />
       <Button clickHandler={() => setBad(bad + 1)} text="bad" />
       <Header name="statistics" />
-      <Counter text="good" count={good} />
-      <Counter text="neutral" count={neutral} />
-      <Counter text="bad" count={bad} />
-      <Counter text="all" count={good + neutral + bad} />
-      <Average value={(good - bad) / (good + neutral + bad)} />
-      <Positive value={good / (good + neutral + bad)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
