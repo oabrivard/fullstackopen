4d1db8de3c52df597c3cd6344b0e87c667cdf720
oabrivard <olivier@abrivard.fr>
2020-04-26 18:59:25 -0400

    finished #2.1: course contents step6

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((acc, curr) => acc + curr.exercises, 0);
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => <Part key={part.name} part={part}/>)}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      {/*<Total course={course} />*/}
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data, 1st part',
+        exercises: 7
+      },
+      {
+        name: 'Using props to pass data, 2nd part',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
