2a27f70e938af0cb09fa53abc19b1a6ed78de286
oabrivard <olivier@abrivard.fr>
2020-04-26 20:22:04 -0400

    finished #2.5: separate module

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,44 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((acc, curr) => acc + curr.exercises, 0);
-  return(
-    <strong>Number of exercises {sum}</strong>
-  ) 
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part}/>)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
-};
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
