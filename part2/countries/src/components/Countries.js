a50693a1df4ba7c23942a4285e0eab40c3da9139
oabrivard <olivier@abrivard.fr>
2020-07-12 15:28:41 -0400

    finished #2.13*: Data for countries, step2

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Country = ({country}) => <div>
   <h1>{country.name}</h1>
@@ -10,23 +10,29 @@ const Country = ({country}) => <div>
 </div>
 
 const Countries = ({countries,newSearch}) => {
+  const [ countryShown, setCountryShown ] = useState(null)
 
-    const filteredCountries = countries.filter(c => c.name.includes(newSearch))
+  const filteredCountries = countries.filter(c => c.name.toLowerCase().includes(newSearch.toLowerCase()))
 
-    if (filteredCountries.length > 10) {
-      return (<div>Too many matches, specify another filter</div>)
-    } else if (filteredCountries.length === 1) {
-      return (<Country country={filteredCountries[0]} />)
-    } else if (filteredCountries.length === 0) {
-      return(<div>No match found</div>)
-    }
+  if (filteredCountries.length > 10) {
+    return (<div>Too many matches, specify another filter</div>)
+  } else if (filteredCountries.length === 1) {
+    return (<Country country={filteredCountries[0]} />)
+  } else if (filteredCountries.length === 0) {
+    return(<div>No match found</div>)
+  }
 
   return (
     <div>
-      {filteredCountries.map(c => <div key={c.name}>{c.name}</div>)}      
+      <div>
+        {filteredCountries.map(c => <div key={c.name}>{c.name} <button onClick={(e) => setCountryShown(c)}>show</button> </div>)}
+      </div>
+      {countryShown!==null &&
+      <div>
+        <Country country={countryShown} />
+      </div>}
     </div>
   );
 };
-
 
 export default Countries;
