fa6534c5eee321fb924a4b65c520d6dc1b3692d0
oabrivard <olivier@abrivard.fr>
2020-08-05 08:21:53 -0400

    finished part3/deploying_app_to_internet

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 
 let notes = [
@@ -77,7 +80,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
