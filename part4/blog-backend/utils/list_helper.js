9fef2c05e375f0607cd6acef6aa003b77ed2d6a6
oabrivard <olivier@abrivard.fr>
2020-08-13 14:12:50 -0400

    finished #4.4: helper functions and unit tests, step2

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -1,7 +1,10 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => blogs.reduce((prev,curr) => prev + curr.likes, 0)
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
