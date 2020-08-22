695bda9d3745390ba504087dc0019e3cc24bb046
oabrivard <olivier@abrivard.fr>
2020-08-22 10:15:34 -0400

    finished #4.8: Blog list tests, step1

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -1,7 +1,4 @@
-const _ = require('lodash');
-const flow = require('lodash/fp/flow');
-const countBy = require('lodash/fp/countBy');
-const reduce = require('lodash/fp/reduce');
+const _ = require('lodash')
 
 const dummy = () => {
   return 1
@@ -11,25 +8,25 @@ const totalLikes = (blogs) => blogs.reduce((prev,curr) => prev + curr.likes, 0)
 
 const favoriteBlog = (blogs) => blogs.length ? blogs.reduce((prev, curr) => (prev.likes > curr.likes) ? prev : curr) : undefined
 
-const mostBlogs = (blogs) => 
+const mostBlogs = (blogs) =>
   _.reduce(
     _.countBy(blogs,'author'),
     (result, value, key) => {
       const blogs = Number(value)
-      return !result || blogs > result.blogs ? {author:key,blogs} : result
-    }, 
+      return !result || blogs > result.blogs ? { author:key,blogs } : result
+    },
     undefined
   )
 
-const mostLikes = (blogs) => 
+const mostLikes = (blogs) =>
   _.maxBy(
     _.map(
       _.groupBy(
         blogs,
         'author'
       ),
-      (blogs,author) => ({author, likes: totalLikes(blogs)})
-    ), 
+      (blogs,author) => ({ author, likes: totalLikes(blogs) })
+    ),
     o => o.likes
   )
 
