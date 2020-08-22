d952879d64a34b7033f2bb7e1ec3bc4cb217d37e
oabrivard <olivier@abrivard.fr>
2020-08-22 09:25:26 -0400

    finished part4/testing_the_backend, part 1

--- part4/notes-backend/controllers/notes.js
+++ part4/notes-backend/controllers/notes.js
@@ -1,18 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      response.json(note)
-    })
-    .catch(error => next(error))
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
@@ -21,6 +12,20 @@ notesRouter.delete('/:id', (request, response, next) => {
       response.status(204).end()
     })
     .catch(error => next(error))
+})
+
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -38,7 +43,7 @@ notesRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -47,9 +52,8 @@ notesRouter.post('/', (request, response, next) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  }).catch(error => next(error))
+  const savedNote = await note.save()
+  response.json(savedNote)
 })
 
 module.exports = notesRouter
